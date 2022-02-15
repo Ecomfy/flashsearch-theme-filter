70d33915695016925f24aa26bcdd7e591a8e2714
Andrew Bien <andrew.bien.bk@gmail.com>
2022-02-15 02:34:40 +0000

    Release 1.3.0

--- js/flashsearch-custom.js
+++ js/flashsearch-custom.js
@@ -1681,6 +1681,15 @@ flashsearch.searchResultsTemplates = {
   <span class="fs-product-sizes__text">{{getVariantSizes(product).join(", ")}}</span>
 </div>
     `,
+  
+  "fs-wishlist": `
+<div v-if="isGrowaveWishlist" :class="'ssw-faveiticon' + ' sswfaveicon' + product.id + ' fs-wishlist fs-wishlist-growave' + ' fs-wishlist-shape-' + shape">
+  <i :data-product-id="product.id" data-count="0"
+        class="ssw-icon-heart-o ssw-fave-icon ssw-wishlist-element ssw-not-synch" :data-params="growave.dataParams" :title="growave.addToWishlistText"></i>
+  <span class="faves-count">...</span>
+</div> 
+<button v-if="isWishlistPlus" :class="'swym-button swym-add-to-wishlist-view-product' + ' fs-wishlist fs-wishlist-wishlistplus' + ' fs-wishlist-shape-' + shape" data-swaction="addToWishlist"  :data-product-id="product.id"></button>
+  `,
 
   "fs-quick-view-item": `
 <fs-modal
@@ -1793,63 +1802,61 @@ flashsearch.searchResultsTemplates = {
               </fs-select-option>
             </fs-select>
           </fs-form-item>
-          <fs-row>
-            <fs-col
-              v-if="isCurrentVariantAvailable && currentVariant.availableForSale"
-              :xl="8" :lg="8" :md="8" :sm="8" :xs="8"
-            >
-              <fs-form-item name="quantity" :label="$t('searchResults.quickView.quantity')">
-                <fs-input-number
-                  size="large"
-                  :min="1"
-                  v-model:value="formState.quantity"
-                  class="fs-quickview__quantity"
-                />
-              </fs-form-item>
-            </fs-col>
-            <fs-col :xl="16" :lg="16" :md="16" :sm="16" :xs="16">
-              <fs-form-item>
-                <fs-button
-                  v-if="isCurrentVariantAvailable && currentVariant.availableForSale"
-                  class="fs-quickview__add-to-cart-btn"
-                  html-type="submit"
-                  size="large"
-                  :loading="qvSubmitLoading"
-                  data-testid="sr-qv-atc-btn"
-                >
-                  {{$t("searchResults.quickView.addToCart")}}
-                </fs-button>
-                <fs-button
-                  v-else-if="isCurrentVariantAvailable && !currentVariant.availableForSale"
-                  size="large"
-                  class="fs-quickview__add-to-cart-btn"
-                  :disabled="true"
-                  data-testid="sr-qv-disabled-btn"
-                >
-                  {{$t("searchResults.quickView.soldOutButton")}}
-                </fs-button>
-                <fs-button
-                  v-else
-                  size="large"
-                  class="fs-quickview__add-to-cart-btn"
-                  :disabled="true"
-                  data-testid="sr-qv-disabled-btn"
-                >
-                  {{$t("searchResults.quickView.unavailable")}}
-                </fs-button>
-              </fs-form-item>
-            </fs-col>
-          </fs-row>
+          <div class="fs-quickview__buttons">
+            <fs-form-item v-if="isCurrentVariantAvailable && currentVariant.availableForSale" name="quantity" :label="$t('searchResults.quickView.quantity')">
+              <fs-input-number
+                size="large"
+                :min="1"
+                v-model:value="formState.quantity"
+                class="fs-quickview__quantity"
+              />
+            </fs-form-item>
+            <fs-form-item>
+              <fs-button
+                v-if="isCurrentVariantAvailable && currentVariant.availableForSale"
+                class="fs-quickview__add-to-cart-btn"
+                html-type="submit"
+                size="large"
+                :loading="qvSubmitLoading"
+                data-testid="sr-qv-atc-btn"
+              >
+                {{$t("searchResults.quickView.addToCart")}}
+              </fs-button>
+              <fs-button
+                v-else-if="isCurrentVariantAvailable && !currentVariant.availableForSale"
+                size="large"
+                class="fs-quickview__add-to-cart-btn"
+                :disabled="true"
+                data-testid="sr-qv-disabled-btn"
+              >
+                {{$t("searchResults.quickView.soldOutButton")}}
+              </fs-button>
+              <fs-button
+                v-else
+                size="large"
+                class="fs-quickview__add-to-cart-btn"
+                :disabled="true"
+                data-testid="sr-qv-disabled-btn"
+              >
+                {{$t("searchResults.quickView.unavailable")}}
+              </fs-button>
+            </fs-form-item>
+            <fs-form-item>
+              <fs-wishlist shape="button" :product="product" :current-variant="currentVariant"/>
+            </fs-form-item>
+          </div>
         </fs-form>
-        <fs-button
-          v-else
-          size="large"
-          class="fs-quickview__add-to-cart-btn"
-          :disabled="true"
-          data-testid="sr-qv-disabled-btn"
-        >
-          {{$t("searchResults.quickView.soldOutButton")}}
-        </fs-button>
+        <div class="fs-quickview__soldout-wrapper" v-else>
+          <fs-button
+            size="large"
+            class="fs-quickview__add-to-cart-btn"
+            :disabled="true"
+            data-testid="sr-qv-disabled-btn"
+          >
+            {{$t("searchResults.quickView.soldOutButton")}}
+          </fs-button>
+          <fs-wishlist shape="button" :product="product" :current-variant="currentVariant"/>
+        </div>
       </div>
     </fs-col>
   </fs-row>
@@ -1872,6 +1879,7 @@ flashsearch.searchResultsTemplates = {
     <div
       :class="'fs-sr-item__image-wrapper' + (borderType === 'around-image' ? ' fs-sr-item-image-bordered' : '') + ' fs-sr-grid-item__image-wrapper'"
     >
+      <fs-wishlist :product="product" :current-variant="currentVariant"/>
       <fs-product-label
         :class="'fs-label--' + productLabelPosition + ' fs-sr-grid-item__product-label'"
         :available-for-sale="product.availableForSale"
@@ -1992,6 +2000,7 @@ flashsearch.searchResultsTemplates = {
           class="fs-sr-item__image-wrapper fs-sr-list-item__image-wrapper"
           :class="'fs-sr-item__image-wrapper' + (borderType === 'around-image' ? ' fs-sr-item-image-bordered' : '') + ' fs-sr-list-item__image-wrapper'"
         >
+          <fs-wishlist :product="product" :current-variant="currentVariant"/>
           <fs-product-label
             :class="'fs-label--' + productLabelPosition + ' fs-sr-list-item__product-label'"
             :available-for-sale="product.availableForSale"
